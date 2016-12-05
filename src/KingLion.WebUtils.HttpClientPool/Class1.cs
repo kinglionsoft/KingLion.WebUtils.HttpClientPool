ac84a82d8145c72bd1671352d523cafc44856de5
yangchao <yangch_ts@qq.com>
2016-12-05 16:07:05 +0800

    添加项目文件。

---
+++ src/KingLion.WebUtils.HttpClientPool/Class1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace KingLion.WebUtils.HttpClientPool
+{
+    public class Class1
+    {
+        public Class1()
+        {
+        }
+    }
+}
